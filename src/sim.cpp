698468d2e027969ab27741d28438895f0f49b737
djapp18 <dennisjacob@icloud.com>
2022-10-04 14:55:55 -0400

    feat: current status

--- src/sim.cpp
+++ src/sim.cpp
@@ -11,13 +11,13 @@
 
 using namespace std;
 
-union REGS 
-{
-    RegisterInfo reg;
-    uint32_t registers[32] {0};
-};
+// union REGS 
+// {
+//     RegisterInfo reg;
+//     uint32_t registers[32] {0};
+// };
 
-union REGS regData;
+// union REGS regData;
 
 enum OP_IDS
 {
@@ -117,7 +117,7 @@ bool checkOverflow(uint32_t sum, uint32_t operandRS, uint32_t operandRT) {
 // dump registers and memory
 // change so registers is a union with reg. No copying of reg to registers - should not be hard
 void dump(MemoryStore *myMem) {
-    dumpRegisterState(regData.reg);
+    //dumpRegisterState(regData.reg);
     dumpMemoryState(myMem);
 }
 
@@ -143,201 +143,201 @@ int main(int argc, char **argv)
         myMem->setMemValue(i * BYTE_SIZE, buf[i], BYTE_SIZE);
     }
 
-    regData.reg = {};
+    // regData.reg = {};
 
-    // initialize registers to 0
-    uint32_t pc = 0; // initialize pc
-    bool err = false;
+    // // initialize registers to 0
+    // uint32_t pc = 0; // initialize pc
+    // bool err = false;
 
-    // branch delay - why diff branch and jal delay
-    bool branchDelay, jalDelay = false;
-    // branchTarget - Tagregt of branch taken
-    // jal_valueToStore -  return address to be stores in reg 31 for JAL instruction
-    uint32_t branchTarget, jal_valueToStore = 0;
+    // // branch delay - why diff branch and jal delay
+    // bool branchDelay, jalDelay = false;
+    // // branchTarget - Tagregt of branch taken
+    // // jal_valueToStore -  return address to be stores in reg 31 for JAL instruction
+    // uint32_t branchTarget, jal_valueToStore = 0;
 
-    while (!err) {
-        // fetch current instruction
-        uint32_t instruct = 0;
-        myMem->getMemValue(pc, instruct, WORD_SIZE);
+    // while (!err) {
+    //     // fetch current instruction
+    //     uint32_t instruct = 0;
+    //     myMem->getMemValue(pc, instruct, WORD_SIZE);
 
-        pc += 4;
-        regData.registers[0] = 0; // reset value of $zero register
+    //     pc += 4;
+    //     regData.registers[0] = 0; // reset value of $zero register
 
-        // dump register and memory before exiting
-        if (instruct == 0xfeedfeed) {
-            dump(myMem);
-            return 0;
-        }
+    //     // dump register and memory before exiting
+    //     if (instruct == 0xfeedfeed) {
+    //         dump(myMem);
+    //         return 0;
+    //     }
 
-        // parse instruction 
-        // complete instructBits function for correct bits for each operand
-        uint32_t opcode = instructBits(instruct, 31, 26);
-        uint32_t rs = instructBits(instruct, 25, 21);
-        uint32_t rt = instructBits(instruct, 20, 16);
-        uint32_t rd = instructBits(instruct, 15, 11);
-        uint32_t shamt = instructBits(instruct, 10, 6);
-        uint32_t funct = instructBits(instruct, 5, 0);
-        uint16_t immediate = instructBits(instruct, 15, 0);
-        uint32_t address = instructBits(instruct, 25, 0);
+    //     // parse instruction 
+    //     // complete instructBits function for correct bits for each operand
+    //     uint32_t opcode = instructBits(instruct, 31, 26);
+    //     uint32_t rs = instructBits(instruct, 25, 21);
+    //     uint32_t rt = instructBits(instruct, 20, 16);
+    //     uint32_t rd = instructBits(instruct, 15, 11);
+    //     uint32_t shamt = instructBits(instruct, 10, 6);
+    //     uint32_t funct = instructBits(instruct, 5, 0);
+    //     uint16_t immediate = instructBits(instruct, 15, 0);
+    //     uint32_t address = instructBits(instruct, 25, 0);
 
-        int32_t signExtImm = signExt(immediate);
-        uint32_t zeroExtImm = zeroSignExt(immediate);
-        uint32_t branchAddr = branchAddress(immediate);
-        uint32_t fallthruAddr = pc; // assumes PC += 4 just happened
+    //     int32_t signExtImm = signExt(immediate);
+    //     uint32_t zeroExtImm = zeroSignExt(immediate);
+    //     uint32_t branchAddr = branchAddress(immediate);
+    //     uint32_t fallthruAddr = pc; // assumes PC += 4 just happened
 
-        // fill in operations for all functions and operands. Account for overflow where necessary.
-        switch(opcode) {
-            case OP_ZERO: // R-type instruction 
-                switch(funct) {
-                    case FUN_ADD:    
-                        regData.registers[rd] = regData.registers[rs] + regData.registers[rt];
-                        err = checkOverflow(regData.registers[rd], regData.registers[rs], regData.registers[rt]);
-                        break;
-                    case FUN_ADDU: 
-                        regData.registers[rd] = regData.registers[rs] + regData.registers[rt];
-                        break;
-                    case FUN_AND: 
-                        regData.registers[rd] = regData.registers[rs] & regData.registers[rt];
-                        break;
-                    case FUN_JR: 
-                        branchDelay = true;
-                        branchTarget = regData.registers[rs];
-                        break;
-                    case FUN_NOR: 
-                        regData.registers[rd] = !(regData.registers[rs] ^ regData.registers[rt]);
-                        break;
-                    case FUN_OR: 
-                        regData.registers[rd] = regData.registers[rs] ^ regData.registers[rt];
-                        break;
-                    case FUN_SLT: 
-                        regData.registers[rd] = ((int32_t) regData.registers[rs] < (int32_t) regData.registers[rt]) ? 1 : 0;
-                        break;
-                    case FUN_SLTU: 
-                        regData.registers[rd] = (regData.registers[rs] < regData.registers[rt]) ? 1 : 0;
-                        break;
-                    case FUN_SLL: 
-                        regData.registers[rd] = regData.registers[rt] << shamt;
-                        break;
-                    case FUN_SRL: 
-                        regData.registers[rd] = regData.registers[rt] >> shamt;
-                        break;
-                    case FUN_SUB:  
-                        regData.registers[rd] = regData.registers[rs] + (~regData.registers[rt] + 1);
-                        err = checkOverflow(regData.registers[rd], regData.registers[rs], regData.registers[rt]);
-                        break;
-                    case FUN_SUBU: 
-                        regData.registers[rd] = regData.registers[rs] + (~regData.registers[rt] + 1);
-                        break;
-                    default:
-                        fprintf(stderr, "\tIllegal operation...\n");
-                        err = true;
-                }
-                break;
+    //     // fill in operations for all functions and operands. Account for overflow where necessary.
+    //     switch(opcode) {
+    //         case OP_ZERO: // R-type instruction 
+    //             switch(funct) {
+    //                 case FUN_ADD:    
+    //                     regData.registers[rd] = regData.registers[rs] + regData.registers[rt];
+    //                     err = checkOverflow(regData.registers[rd], regData.registers[rs], regData.registers[rt]);
+    //                     break;
+    //                 case FUN_ADDU: 
+    //                     regData.registers[rd] = regData.registers[rs] + regData.registers[rt];
+    //                     break;
+    //                 case FUN_AND: 
+    //                     regData.registers[rd] = regData.registers[rs] & regData.registers[rt];
+    //                     break;
+    //                 case FUN_JR: 
+    //                     branchDelay = true;
+    //                     branchTarget = regData.registers[rs];
+    //                     break;
+    //                 case FUN_NOR: 
+    //                     regData.registers[rd] = !(regData.registers[rs] ^ regData.registers[rt]);
+    //                     break;
+    //                 case FUN_OR: 
+    //                     regData.registers[rd] = regData.registers[rs] ^ regData.registers[rt];
+    //                     break;
+    //                 case FUN_SLT: 
+    //                     regData.registers[rd] = ((int32_t) regData.registers[rs] < (int32_t) regData.registers[rt]) ? 1 : 0;
+    //                     break;
+    //                 case FUN_SLTU: 
+    //                     regData.registers[rd] = (regData.registers[rs] < regData.registers[rt]) ? 1 : 0;
+    //                     break;
+    //                 case FUN_SLL: 
+    //                     regData.registers[rd] = regData.registers[rt] << shamt;
+    //                     break;
+    //                 case FUN_SRL: 
+    //                     regData.registers[rd] = regData.registers[rt] >> shamt;
+    //                     break;
+    //                 case FUN_SUB:  
+    //                     regData.registers[rd] = regData.registers[rs] + (~regData.registers[rt] + 1);
+    //                     err = checkOverflow(regData.registers[rd], regData.registers[rs], regData.registers[rt]);
+    //                     break;
+    //                 case FUN_SUBU: 
+    //                     regData.registers[rd] = regData.registers[rs] + (~regData.registers[rt] + 1);
+    //                     break;
+    //                 default:
+    //                     fprintf(stderr, "\tIllegal operation...\n");
+    //                     err = true;
+    //             }
+    //             break;
 
-            case OP_ADDI: 
-                regData.registers[rt] = regData.registers[rs] + signExtImm;
-                err = checkOverflow(regData.registers[rd], regData.registers[rs], signExtImm);
-                break;                
-            case OP_ADDIU: 
-                regData.registers[rt] = regData.registers[rs] + signExtImm;
-                break;
-            case OP_ANDI: 
-                regData.registers[rt] = regData.registers[rs] & zeroExtImm;
-                break;
-            case OP_BEQ: // make sure to implement logic about executign instruction immediately afterwards
-                if(regData.registers[rs] == regData.registers[rt]) {
-                    branchDelay = true;
-                    branchTarget = pc + branchAddr;
-                }
-                break;
-            case OP_BNE: 
-                if(regData.registers[rs] != regData.registers[rt]) {
-                    branchDelay = true;
-                    branchTarget = pc + branchAddr;
-                }
-                break;      
-            case OP_J: 
-                branchDelay = true;
-                uint32_t extend_address = address << 2;  
-                uint32_t region = instructBits(pc, 31, 28) << 28;
-                branchTarget = extend_address ^ region;
-                break;
-            case OP_JAL:
-                branchDelay = true; 
-                regData.registers[31] = pc + 4;
-                uint32_t extend_address = address << 2;  
-                uint32_t region = instructBits(pc, 31, 28) << 28;
-                branchTarget = extend_address ^ region;
-                break;
-            case OP_LBU: 
-                uint32_t addr = regData.registers[rs] + signExtImm;
-                uint32_t value = 0;
-                myMem->getMemValue(addr, value, BYTE_SIZE);
-                regData.registers[rt] = value;
-                break;
-            case OP_LHU: 
-                uint32_t addr = regData.registers[rs] + signExtImm;
-                uint32_t value = 0;
-                myMem->getMemValue(addr, value, HALF_SIZE);
-                regData.registers[rt] = value;
-                break;
-            case OP_LUI: 
-                regData.registers[rt] = immediate << 16;    
-                break;   
-            case OP_LW: 
-                uint32_t addr = regData.registers[rs] + signExtImm;
-                uint32_t value = 0;
-                myMem->getMemValue(addr, value, WORD_SIZE);
-                regData.registers[rt] = value;  
-                break;         
-            case OP_ORI: 
-                regData.registers[rt] = regData.registers[rs] | zeroExtImm;
-                break;
-            case OP_SLTI: 
-                regData.registers[rt] = ((int32_t) regData.registers[rs] < (int32_t) signExtImm) ? 1 : 0;
-                break;
-            case OP_SLTIU: 
-                regData.registers[rt] = (regData.registers[rs] < signExtImm) ? 1 : 0;
-                break;
-            case OP_SB: 
-                uint32_t addr = regData.registers[rs] + signExtImm;
-                myMem->setMemValue(addr, regData.registers[rt], BYTE_SIZE);
-                break;
-            case OP_SH: 
-                uint32_t addr = regData.registers[rs] + signExtImm;
-                myMem->setMemValue(addr, regData.registers[rt], HALF_SIZE); 
-                break;
-            case OP_SW: 
-                uint32_t addr = regData.registers[rs] + signExtImm;
-                myMem->setMemValue(addr, regData.registers[rt], WORD_SIZE) ;  
-                break;
-            case OP_BLEZ: 
-                if(regData.registers[rs] <= 0) {
-                    branchDelay = true;
-                    branchTarget = pc + branchAddr;
-                }
-                break;   
-            case OP_BGTZ: 
-                if(regData.registers[rs] > 0) {
-                    branchDelay = true;
-                    branchTarget = pc + branchAddr;
-                }
-                break;
-            default:
-                fprintf(stderr, "\tIllegal operation...\n");
-                err = true;
-                break;
-        }
+    //         case OP_ADDI: 
+    //             regData.registers[rt] = regData.registers[rs] + signExtImm;
+    //             err = checkOverflow(regData.registers[rd], regData.registers[rs], signExtImm);
+    //             break;                
+    //         case OP_ADDIU: 
+    //             regData.registers[rt] = regData.registers[rs] + signExtImm;
+    //             break;
+    //         case OP_ANDI: 
+    //             regData.registers[rt] = regData.registers[rs] & zeroExtImm;
+    //             break;
+    //         case OP_BEQ: // make sure to implement logic about executign instruction immediately afterwards
+    //             if(regData.registers[rs] == regData.registers[rt]) {
+    //                 branchDelay = true;
+    //                 branchTarget = pc + branchAddr;
+    //             }
+    //             break;
+    //         case OP_BNE: 
+    //             if(regData.registers[rs] != regData.registers[rt]) {
+    //                 branchDelay = true;
+    //                 branchTarget = pc + branchAddr;
+    //             }
+    //             break;      
+    //         case OP_J: 
+    //             branchDelay = true;
+    //             uint32_t extend_address = address << 2;  
+    //             uint32_t region = instructBits(pc, 31, 28) << 28;
+    //             branchTarget = extend_address ^ region;
+    //             break;
+    //         case OP_JAL:
+    //             branchDelay = true; 
+    //             regData.registers[31] = pc + 4;
+    //             uint32_t extend_address = address << 2;  
+    //             uint32_t region = instructBits(pc, 31, 28) << 28;
+    //             branchTarget = extend_address ^ region;
+    //             break;
+    //         case OP_LBU: 
+    //             uint32_t addr = regData.registers[rs] + signExtImm;
+    //             uint32_t value = 0;
+    //             myMem->getMemValue(addr, value, BYTE_SIZE);
+    //             regData.registers[rt] = value;
+    //             break;
+    //         case OP_LHU: 
+    //             uint32_t addr = regData.registers[rs] + signExtImm;
+    //             uint32_t value = 0;
+    //             myMem->getMemValue(addr, value, HALF_SIZE);
+    //             regData.registers[rt] = value;
+    //             break;
+    //         case OP_LUI: 
+    //             regData.registers[rt] = immediate << 16;    
+    //             break;   
+    //         case OP_LW: 
+    //             uint32_t addr = regData.registers[rs] + signExtImm;
+    //             uint32_t value = 0;
+    //             myMem->getMemValue(addr, value, WORD_SIZE);
+    //             regData.registers[rt] = value;  
+    //             break;         
+    //         case OP_ORI: 
+    //             regData.registers[rt] = regData.registers[rs] | zeroExtImm;
+    //             break;
+    //         case OP_SLTI: 
+    //             regData.registers[rt] = ((int32_t) regData.registers[rs] < (int32_t) signExtImm) ? 1 : 0;
+    //             break;
+    //         case OP_SLTIU: 
+    //             regData.registers[rt] = (regData.registers[rs] < signExtImm) ? 1 : 0;
+    //             break;
+    //         case OP_SB: 
+    //             uint32_t addr = regData.registers[rs] + signExtImm;
+    //             myMem->setMemValue(addr, regData.registers[rt], BYTE_SIZE);
+    //             break;
+    //         case OP_SH: 
+    //             uint32_t addr = regData.registers[rs] + signExtImm;
+    //             myMem->setMemValue(addr, regData.registers[rt], HALF_SIZE); 
+    //             break;
+    //         case OP_SW: 
+    //             uint32_t addr = regData.registers[rs] + signExtImm;
+    //             myMem->setMemValue(addr, regData.registers[rt], WORD_SIZE) ;  
+    //             break;
+    //         case OP_BLEZ: 
+    //             if(regData.registers[rs] <= 0) {
+    //                 branchDelay = true;
+    //                 branchTarget = pc + branchAddr;
+    //             }
+    //             break;   
+    //         case OP_BGTZ: 
+    //             if(regData.registers[rs] > 0) {
+    //                 branchDelay = true;
+    //                 branchTarget = pc + branchAddr;
+    //             }
+    //             break;
+    //         default:
+    //             fprintf(stderr, "\tIllegal operation...\n");
+    //             err = true;
+    //             break;
+    //     }
 
-        if(branchDelay) {
-            pc = branchTarget;
-            branchDelay = false;
-        }
+    //     if(branchDelay) {
+    //         pc = branchTarget;
+    //         branchDelay = false;
+    //     }
 
-    }
+    // }
 
-    // dump and exit with error
-    dump(myMem);
-    exit(127);
-    return -1;
+    // // dump and exit with error
+    // dump(myMem);
+    // exit(127);
+    // return -1;
 }
